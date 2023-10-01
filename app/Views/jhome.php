7ed428678ec1d88524082ef535ee7365a9717cbb
FajilanJennyRose <144981883+KarlHensonBalita@users.noreply.github.com>
2023-10-01 20:17:57 +0800

    initial commit

---
+++ app/Views/jhome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<?= view('inc/head'); ?>
+<body>
+<?= view('inc/loder'); ?> 
+<?= view('inc/cute'); ?> 
+<?= view('inc/about'); ?> 
+<?= view('inc/client'); ?> 
+<?= view('inc/contact'); ?> 
+</body>
+</html>
